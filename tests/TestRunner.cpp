4c90468a65cb8caaece95c0e58b2eba5ef395471
Leodosin <lleijona@yahoo.com>
2023-10-23 15:18:31 +0300

    WIP: add tests for object class

---
+++ tests/TestRunner.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    // Run your tests here
+    std::cout << "Running ObjectTests..." << std::endl;
+    int objectTestsResult = system("ObjectTests.exe");  // Replace "ObjectTests.exe" with the actual name of your test executable.
+
+    // You can add more test runners for other classes in your project.
+
+    if (objectTestsResult == 0) {
+        std::cout << "All tests passed." << std::endl;
+    } else {
+        std::cout << "Some tests failed." << std::endl;
+    }
+
+    return objectTestsResult;
+}
