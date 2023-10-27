117c62309aeac3bc4c4e33c45846c254d40b5517
Mikhail Romanov <mikhail.romanov@aalto.fi>
2023-10-27 19:59:40 +0300

    added descriptions of the towers and enemies to the plan

---
+++ build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/src/level.cpp.o"
+  "CMakeFiles/test.dir/src/level.cpp.o.d"
+  "CMakeFiles/test.dir/src/main.cpp.o"
+  "CMakeFiles/test.dir/src/main.cpp.o.d"
+  "CMakeFiles/test.dir/src/object.cpp.o"
+  "CMakeFiles/test.dir/src/object.cpp.o.d"
+  "CMakeFiles/test.dir/src/square.cpp.o"
+  "CMakeFiles/test.dir/src/square.cpp.o.d"
+  "test"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
