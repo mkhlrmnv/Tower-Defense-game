22c565f3e0e9f8f29d6df2aaf6b36026c836fed1
Leo Teodosin <leo.teodosin@aalto.fi>
2023-11-06 15:16:13 +0200

    Create attack function for basic enemy

--- src/basic_enemy.cpp
+++ src/basic_enemy.cpp
@@ -1,8 +1,30 @@
 #include "basic_enemy.hpp"
 
+#include <chrono>
+#include <thread>
+
 Basic_Enemy::Basic_Enemy() :
     Enemy(50, 10, 20, 100, Vector2D(0,0), NEUTRAL, 10, ) {}
 
 Basic_Enemy::Basic_Enemy(int health, int damage, int range, int attack_speed, Vector2D& position, int type, int speed, int defense) :
     Enemy(health, damage, range, attack_speed, position, type, speed, defense) {}
 
+void Basic_Enemy::attack() {
+    double multiplier;
+    auto closestObjects = distances();
+
+    if (!closestObjects.empty()) {
+        if (closestObjects[0].first < this->get_range()) {
+            multiplier = check_type_multiplier(this, closestObjects[0].second);
+            closestObjects[0].second->lose_health(this->get_damage() * multiplier);
+        } else {
+            for (int i = 0; i < 3; ++i) {
+                multiplier = check_type_multiplier(this, closestObjects[i].second);
+                if (closestObjects[i].first < this->get_range()) {
+                    closestObjects[i].second->lose_health(this->get_damage() * multiplier);
+                }
+            }
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000 - this->get_attack_speed()));
+    }
+}
