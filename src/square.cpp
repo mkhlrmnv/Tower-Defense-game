af841de7549694151b8f65d56463d200fc75e74c
Mikhail Romanov <mikhail.romanov@aalto.fi>
2023-11-06 16:13:50 +0200

    added enemy and towers list to level

--- src/square.cpp
+++ src/square.cpp
@@ -9,21 +9,11 @@ Vector2D Square::get_center() const {
     return _center;
 }
 
-// Returns list of all objects
-std::vector<Object*> Square::get_objects() const{
-    return _objects;
-}
-
 // Returns who occupies square 
 int Square::get_occupied() const{
     return _occupied_by;
 }
 
-// add object to square
-void Square::add_object(Object *object){
-    _objects.push_back(object);
-    //Update occupied_by
-}
 
 // occupies square by grass
 bool Square::occupy_by_grass(){ 
@@ -31,7 +21,6 @@ bool Square::occupy_by_grass(){
         return false;
     }
     _occupied_by = grass;
-    _objects.clear();  // deletes tower from object list
     return true;
 }
 
