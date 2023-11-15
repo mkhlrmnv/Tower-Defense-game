b8729c4ef7e07a84cb862e447875e7702f165021
Mikhail Romanov <mikhail.romanov@aalto.fi>
2023-11-15 14:49:56 +0200

    pulled working move function from object branch

--- src/basic_enemy.cpp
+++ src/basic_enemy.cpp
@@ -21,3 +21,45 @@ void Basic_Enemy::attack() {
         std::this_thread::sleep_for(std::chrono::milliseconds(this->get_attack_speed()));
     }
 }
+
+bool Basic_Enemy::move() {
+    Vector2D next_position;
+    for (Direction direction : get_level_reference().next_road(this)) {
+        if (direction == up) {
+            next_position = get_level_reference().get_grid()[get_position().x][get_position().y - get_level_reference().get_square_size()]->get_center();
+            for (auto pos : get_route()) {
+                if (pos != next_position) {
+                    set_target_position(next_position);
+                    return true;
+                }
+            }
+            
+        } else if (direction == down) {
+            next_position = get_level_reference().get_grid()[get_position().x][get_position().y + get_level_reference().get_square_size()]->get_center();
+            for (auto pos : get_route()) {
+                if (pos != next_position) {
+                    set_target_position(next_position);
+                    return true;
+                }
+            }
+        } else if (direction == right) {
+            next_position = get_level_reference().get_grid()[get_position().x + get_level_reference().get_square_size()][get_position().y]->get_center();
+            for (auto pos : get_route()) {
+                if (pos != next_position) {
+                    set_target_position(next_position);
+                    return true;
+                }
+            }
+            
+        } else if (direction == left) {
+            next_position = get_level_reference().get_grid()[get_position().x - get_level_reference().get_square_size()][get_position().y]->get_center();
+            for (auto pos : get_route()) {
+                if (pos != next_position) {
+                    set_target_position(next_position);
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
