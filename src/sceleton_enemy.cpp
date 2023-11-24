0a6e026b34fb00345b658d37c8201d5d24417c09
Mikhail Romanov <mikhail.romanov@aalto.fi>
2023-11-24 12:11:03 +0200

    deleted all new files for troubel shooting

--- src/sceleton_enemy.cpp
+++
@@ -1,9 +0,0 @@
-#include "sceleton_enemy.hpp"
-#include "level.hpp"
-
-Sceleton::Sceleton(Level& level, Vector2D& position, int health, int damage, int range, int attack_speed, int type, int speed, int defense) :
-    Enemy(level, position, health, damage, range, attack_speed, type, speed, defense) {}
-
-bool Sceleton::attack() {
-    return false;
-}
