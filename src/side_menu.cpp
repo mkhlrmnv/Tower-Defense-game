5064ce468aa9c3cb275e5983d350c011eacedc91
klind <kalle.lindgren@aalto.fi>
2023-11-26 19:52:20 +0200

    Advance in sidemenu features, adjust buttons, add features to rh

--- src/side_menu.cpp
+++ src/side_menu.cpp
@@ -15,7 +15,8 @@ SideMenu::SideMenu(float game_resolution, float side_menu_width, ResourceHandler
     
     setup_background();
     setup_info_displays();
-    setup_buttons();
+    setup_drag_buttons();
+    setup_round_button();
     setup_menu_title();
 }
 
@@ -34,7 +35,7 @@ void SideMenu::setup_menu_title(){
 
     auto brown = sf::Color(121, 85, 72);
 
-    std::string title_text = "Drag towers to grids";
+    std::string title_text = "Drag Towers to grid!";
     
     float buffer_x = 5;
     float buffer_y = 20;
@@ -44,13 +45,13 @@ void SideMenu::setup_menu_title(){
     
     _title.setFont(_rh.get_font());
     _title.setString(title_text);
-    _title.setCharacterSize(30);
+    _title.setCharacterSize(25);
     _title.setFillColor(brown);
     _title.setPosition({x + buffer_x, y + buffer_y});
 
 }
 
-void SideMenu::setup_buttons(){
+void SideMenu::setup_drag_buttons(){
 
     float x_wrt_bg  = 10;
     float y_wrt_bg  = 100;
@@ -66,33 +67,104 @@ void SideMenu::setup_buttons(){
         auto tower_name = _rh.get_tower_name(i);
         auto tower_texture = _rh.get_texture_tower(i);
         auto attrs = _rh.get_tower_info(i);
+       
 
-        _drag_buttons.push_back(new TowerDragButton(i, tower_name, {x1 +(130+20)*(i%2), y1 + (180 + 20)*(i%3)}, tower_texture, _outline_color, _fill_color, attrs));
-        _drag_buttons.at(i)->set_font(_rh.get_font());
-    }
-    
+        _drag_buttons.push_back(new TowerDragButton(i, _rh.get_tower_name(i), {x1 +(130+20)*(i%2), y1 + (180 + 20)*(i%3)}, _rh.get_texture_tower(i), _outline_color, _fill_color, _rh.get_tower_info(i), &_rh.get_font(), _rh));
+    }   
 
 }
 
+void SideMenu::setup_round_button(){
+
+    // get position of the last button 
+
+   _round_button =  new Button("IMPLEMENT FUNCTIONALITY", {280, 50}, {810, 700}, sf::Color::Red, sf::Color::White, &_rh.get_font());
+   _round_button->center_text();
+
+}
+
+void SideMenu::setup_info_display(int type, sf::Sprite& sprite, sf::Text& text_obj, sf::Vector2f pos, float char_size){
+
+    auto brown = sf::Color(121, 85, 72);
+    auto color = brown;
+    
+    float image_size = 80;
+    float scale = 30 / image_size;
+    sprite.setTexture(_rh.get_texture_attribute(type));
+    sprite.setPosition(pos);
+    auto s = sprite.getScale();
+        std::cout <<"scale x"<< s.x << "scale y" << s.y << std::endl;
+
+
+    sprite.setScale(scale, scale);
+    
+
+    text_obj.setFont(_rh.get_font());
+    text_obj.setCharacterSize(char_size);
+    text_obj.setFillColor(sf::Color::White);
+    text_obj.setOutlineThickness(2);
+    text_obj.setOutlineColor(brown);
+    
+    auto img_size = sprite.getGlobalBounds();
+    auto img_pos = sprite.getPosition();
+    float gap_to_image = 5;
+    float y_offset_to_image = 5;
+
+    text_obj.setPosition( img_pos.x + img_size.width + gap_to_image, img_pos.y - y_offset_to_image);
+
+}
 
 void SideMenu::setup_info_displays(){
 
+
+    float buffer = 5; 
+    float char_size = 30;
+
+    float side_menu_width = _background.getGlobalBounds().width;
+    float side_menu_height = _background.getGlobalBounds().height;
+
+    
+    int info_display_height = char_size;
+    int info_display_width = side_menu_width / 3;
+
+    float display_start_x = _background.getGlobalBounds().left + buffer;
+    float display_start_y =  side_menu_height - info_display_height - buffer;
+
+    float l_x = display_start_x;
+    float l_y = display_start_y;
+
+    setup_info_display(TowerAttributes::HP, _lives_drawable, _lives_text, {l_x, l_y}, char_size);
+
+    float c_x =  l_x + info_display_width;
+    float c_y =  display_start_y;
+
+    setup_info_display(TowerAttributes::MONEY, _cash_drawable, _cash_text, {c_x, c_y}, char_size);
+
+    float r_x =  c_x + info_display_width;
+    float r_y =  display_start_y;
+
+    float extra_space = 10;
+
+    setup_info_display(TowerAttributes::ATKSPD, _round_count_drawable, _round_count_text, {r_x + extra_space, r_y}, char_size); // TODO: image for rounds? 
+
+}
+
+/* 
+void SideMenu::setup_info_displays(){
     
     sf::Color ashgrey(178, 190, 181); 
     auto text_color = ashgrey;
 
     // raise the displays from the bottom some pixels
     float buffer = 5; 
+    float char_size = 30;
 
     float display_start_x = _background.getGlobalBounds().left;
     float side_menu_width = _background.getGlobalBounds().width;
     float side_menu_height = _background.getGlobalBounds().height;
 
-    _cash_text.setFont(_rh.get_font());
-    _lives_text.setFont(_rh.get_font());
-    _round_count_text.setFont(_rh.get_font());
     
-    int info_display_height = 20;
+    int info_display_height = char_size;
     int info_display_width = side_menu_width / 3;
 
     int cash_x = display_start_x;
@@ -100,23 +172,23 @@ void SideMenu::setup_info_displays(){
 
     _cash_text.setPosition(sf::Vector2f(cash_x, cash_y));
     _cash_text.setFillColor(text_color);
-    _cash_text.setCharacterSize(20);
+    _cash_text.setCharacterSize(char_size);
 
     int lives_x = cash_x + info_display_width;
     int lives_y = side_menu_height - info_display_height - buffer;
 
     _lives_text.setPosition(sf::Vector2f(lives_x, lives_y));
     _lives_text.setFillColor(text_color);
-    _lives_text.setCharacterSize(20);
+    _lives_text.setCharacterSize(char_size);
 
     int round_count_x = lives_x + info_display_width;
     int round_count_y = side_menu_height - info_display_height - buffer;
 
     _round_count_text.setPosition(sf::Vector2f(round_count_x, round_count_y));
     _round_count_text.setFillColor(text_color);
-    _round_count_text.setCharacterSize(20);
+    _round_count_text.setCharacterSize(char_size);
 
-}
+}*/
 
 
 
@@ -125,7 +197,21 @@ void SideMenu::draw( sf::RenderTarget& target, sf::RenderStates states) const{
     target.draw(_cash_text);
     target.draw(_lives_text);
     target.draw(_round_count_text);
+
     target.draw(_title);
+    target.draw(*_round_button);
+
+    if(nullptr == _lives_drawable.getTexture()){
+        std::cout <<"no texture"<< std::endl;
+    }
+    //std::cout <<"x"<<  _lives_drawable.getPosition().x << "y" << _lives_drawable.getPosition().y << std::endl;
+    //std::cout <<"w"<<  _lives_drawable.getGlobalBounds().width << "h" << _lives_drawable.getGlobalBounds().height << std::endl;
+
+
+    target.draw(_lives_drawable);
+    target.draw(_cash_drawable);
+    target.draw(_round_count_drawable);
+
     for (auto drag_button: _drag_buttons){ 
         target.draw(*drag_button);
     }
@@ -140,18 +226,17 @@ void SideMenu::draw( sf::RenderTarget& target, sf::RenderStates states) const{
 void SideMenu::handle_events(sf::RenderWindow& window, const sf::Event& event){
     for(auto drag_button : _drag_buttons){
         drag_button->handle_events(window, event, _level);
-        std::cout <<"type:" << drag_button->get_type() << " dragging: " << drag_button->get_drag_flag() << std::endl; ;
+        //std::cout <<"type:" << drag_button->get_type() << " dragging: " << drag_button->get_drag_flag() << std::endl; ;
         _drag_img_ptrs[drag_button->get_type()] =  drag_button->get_dragging_image();
     }
+    _round_button->handle_events(window, event, _level);
 }
 
-void SideMenu::update_displays(){
+void SideMenu::update(){
 
-    std::string cash = "$" + std::to_string(_level.get_cash());
-    std::string lives = "Lives: " + std::to_string(_level.get_lives());
-    std::string round_count = "Round: " + std::to_string(_level.get_round());
-
-
+    std::string cash = std::to_string(_level.get_cash());
+    std::string lives = std::to_string(_level.get_lives());
+    std::string round_count = std::to_string(_level.get_round());
 
     _cash_text.setString(cash);
     _lives_text.setString(lives);
--- src/tower_drag_button.cpp
+++ src/tower_drag_button.cpp
@@ -1,16 +1,21 @@
 #include "tower_drag_button.hpp"
 #include <iostream>
 
-TowerDragButton::TowerDragButton(int type, const std::string name, sf::Vector2f position,  sf::Texture obj_texture, sf::Color outline, sf::Color fill, const std::array<int, 5>& tower_atrs) : 
+TowerDragButton::TowerDragButton(int type, const std::string& name, sf::Vector2f position,  
+sf::Texture obj_texture, sf::Color outline, sf::Color fill, std::array<int, 5>& tower_attrs, const sf::Font* font, ResourceHandler& rh): 
 
-Button(name, {130, 180}, position, fill, outline), _tower_type(type), _texture(obj_texture), _attributes(tower_atrs), _drag_flag(false){
+Button(name, {130, 180}, position, fill, outline, font), 
+    _tower_type(type), 
+    _texture(obj_texture), 
+    _attributes(tower_attrs), 
+    _drag_flag(false), 
+    _tower_name(name),
+    _rh(rh){
 
     // set the first frame of the spreadsheet to represent tower as 
-    //_drawable_tower.setTextureRect(sf::IntRect((frame + animation_pos) * 32, 0 * 32, 32, 32));
-
-    
+    //_drawable_tower.setTextureRect(sf::IntRect(d(frame + animation_pos) * 32, 0 * 32, 32, 32));
+    set_font();
     _button.setOutlineThickness(2);
-
 
     float image_size = 32;
 
@@ -41,7 +46,6 @@ Button(name, {130, 180}, position, fill, outline), _tower_type(type), _texture(o
     _img_background.setSize({80, 80});
     
 
-
     // same for the dragging tower image
 
     _drawable_dragging_tower.setTexture(_texture);
@@ -50,22 +54,170 @@ Button(name, {130, 180}, position, fill, outline), _tower_type(type), _texture(o
 
 
     // TODO: set price and other attributes
-    /* 
-    float text_size = _text.getCharacterSize();
-    _button.setSize({tx, ty + text_size});
-    _size = sf::Vector2f(tx, ty + text_size);
 
-    float buffer = 5;
 
-    float text_x = _drawable_tower.getPosition().x + _button.getGlobalBounds().width / 2 ; - _text.getLocalBounds().width/2;
-    float text_y = _drawable_tower.getPosition().y + _drawable_tower.getGlobalBounds().height ; // - _text.getCharacterSize() - buffer;
+    setup_button_texts();
+    setup_attribute_images();
+}
 
-    _text.setPosition({text_x, text_y});
+
+
+
+void TowerDragButton::setup_button_texts(){
     
-    */
+    auto brown = sf::Color(121, 85, 72);
+
+    // tower name - use inherited button text
+    auto pos = _button.getPosition();
+
+    float title_x = pos.x  + _button.getGlobalBounds().width / 2 - _text.getGlobalBounds().width / 2.f;
+    float title_y = pos.y;
+
+    std::cout << "x"<<title_x << "" << title_y << std::endl; 
+
+    //_text.setOrigin(_text.getGlobalBounds().getSize() / 2.f + _text.getGlobalBounds().getPosition());
+    _text.setPosition({title_x, title_y});
+
+
+    std::cout << "_text.getGlobalBounds().height " << _text.getGlobalBounds().height << std::endl;
+    std::cout << "_text.getGlobalBounds().width " << _text.getGlobalBounds().width << std::endl;
+    std::cout << "_text.getGlobalBounds().left " << _text.getGlobalBounds().left << std::endl;
+    std::cout << "_text.getGlobalBounds().top " << _text.getGlobalBounds().top << std::endl;
+
+    _text.setFillColor(brown);
+    _text.setCharacterSize(15);
+    _text.setString(_tower_name);
+
+    _price_text.setPosition({0,0});
+
+
     
+
+    float price_char_size = 20; 
+    
+    
+
+    _price_text.setCharacterSize(price_char_size);
+    _price_text.setFillColor(sf::Color::White);
+    _price_text.setOutlineColor(sf::Color::Black);
+    _price_text.setOutlineThickness(1);
+    _price_text.setString(std::to_string(_attributes.at(4))+"$");  
+
+    _price_text.setOrigin(_price_text.getGlobalBounds().getSize() / 2.f);
+
+    float price_x = _img_background.getPosition().x + _img_background.getGlobalBounds().width/2;
+    float price_y = _img_background.getPosition().y + _img_background.getGlobalBounds().height - price_char_size;
+    
+    
+    std::cout << "_price_text.getGlobalBounds().height " << _price_text.getGlobalBounds().height << std::endl;
+    std::cout << "_price_text.getGlobalBounds().width " << _price_text.getGlobalBounds().width << std::endl;
+    std::cout << "_price_text.getGlobalBounds().left " << _price_text.getGlobalBounds().left << std::endl;
+    std::cout << "_price_text.getGlobalBounds().top " << _price_text.getGlobalBounds().top << std::endl;    
+
+    
+    _price_text.setPosition({price_x, price_y});
+
+
+    
+
+    // modify these
+    float char_size = 18;
+    float line_spacing = 2;
+    float center_point = _img_background.getGlobalBounds().width/2;
+    float attr_start_x = _img_background.getPosition().x + center_point;
+    float attr_start_y = _img_background.getPosition().y +_drawable_tower.getGlobalBounds().height;
+
+    float hp_x = attr_start_x;
+    float hp_y = attr_start_y;
+
+    _hp_text.setCharacterSize(char_size);
+    _hp_text.setFillColor(brown);
+    _hp_text.setString(std::to_string(_attributes.at(0)));
+
+    _hp_text.setPosition({hp_x, hp_y});
+
+    float dmg_x = attr_start_x;
+    float dmg_y = hp_y + char_size + line_spacing;
+
+    _dmg_text.setCharacterSize(char_size);
+    _dmg_text.setFillColor(brown);
+    _dmg_text.setString(std::to_string(_attributes.at(1))); 
+
+    _dmg_text.setPosition({dmg_x, dmg_y});
+
+    float rng_x = attr_start_x ;
+    float rng_y = dmg_y + char_size + line_spacing;
+
+    _rng_text.setCharacterSize(char_size);
+    _rng_text.setFillColor(brown);
+    _rng_text.setString(std::to_string(_attributes.at(2)));
+
+    _rng_text.setPosition({rng_x, rng_y});
+
+    float atkspd_x = attr_start_x ;
+    float atkspd_y = rng_y + char_size + line_spacing;  
+
+    _atkspd_text.setCharacterSize(char_size);
+    _atkspd_text.setFillColor(brown);
+    _atkspd_text.setString(std::to_string(_attributes.at(3)));
+    
+    _atkspd_text.setPosition({atkspd_x, atkspd_y});
+}
+
+void TowerDragButton::set_font(){
+    _price_text.setFont(*_font);
+    _hp_text.setFont(*_font);
+    _dmg_text.setFont(*_font);
+    _rng_text.setFont(*_font);
+    _atkspd_text.setFont(*_font);
+
 
 }
+
+void TowerDragButton::setup_attribute_images(){
+
+    float start_y_offset = 5;
+    float start_x_offset = 20;
+    float img_size = 15;
+    float line_spacing = 5;
+
+    float attr_start_x = _img_background.getPosition().x + start_x_offset;
+    float attr_start_y = _img_background.getPosition().y +_drawable_tower.getGlobalBounds().height;
+
+    float hp_x = attr_start_x ;
+    float hp_y = attr_start_y + start_y_offset ;
+
+    set_attribute_image(TowerAttributes::HP, _hp_img, {hp_x, hp_y});
+
+    float dmg_x = attr_start_x;
+    float dmg_y = hp_y + img_size + line_spacing;
+
+    set_attribute_image(TowerAttributes::RNG, _dmg_img, {dmg_x, dmg_y}); // TODO: get image 
+
+    float rng_x = attr_start_x;
+    float rng_y = dmg_y + img_size + line_spacing;
+
+    set_attribute_image(TowerAttributes::RNG, _rng_img, {rng_x, rng_y});
+
+    float atkspd_x = attr_start_x;
+    float atkspd_y = rng_y + img_size + line_spacing;
+
+    set_attribute_image(TowerAttributes::ATKSPD, _atkspd_img, {atkspd_x, atkspd_y});
+
+
+
+}
+
+void TowerDragButton::set_attribute_image(int type, sf::Sprite& sprite, sf::Vector2f pos ){
+    
+    sprite.setTexture(_rh.get_texture_attribute(type));
+    sprite.setPosition(pos);
+
+    float image_size = 80;
+    float scale = 16 / image_size;
+    sprite.setScale(scale, scale);
+}
+
 
 void TowerDragButton::set_drag_flag(){
     _drag_flag = true;
@@ -165,10 +317,32 @@ void TowerDragButton::handle_events(sf::RenderWindow& window, const sf::Event& e
 
 void TowerDragButton::draw(sf::RenderTarget& target, sf::RenderStates) const {
 
-    
     target.draw(_button);
     target.draw(_img_background);
     target.draw(_drawable_tower);
+
+    // texts
     target.draw(_text);
+    target.draw(_price_text);
+    target.draw(_hp_text);
+    target.draw(_dmg_text);
+    target.draw(_rng_text);
+    target.draw(_atkspd_text);
+
+    // attribute images
+
+    target.draw(_hp_img);
+    target.draw(_dmg_img);
+    target.draw(_rng_img);
+    target.draw(_atkspd_img);
+
+
+
+
+    // Draw in side menu instead so dragging image appears at top of everything
+    /* 
+    if(_drag_flag){
+        target.draw(_drawable_dragging_tower);
+    }*/
 }
 
