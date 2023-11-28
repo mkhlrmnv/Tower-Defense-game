d2fe6e3decaa2a5f6ae3d292fb222114fa5b8425
Mikhail Romanov <mikhail.romanov@aalto.fi>
2023-11-28 20:27:53 +0200

    Uncommented level test and modified to use new constructors of enemy and tower Also added tests for all new functions in level class

--- tests/SquareTests.cpp
+++ tests/SquareTests.cpp
@@ -1,43 +1,43 @@
-// #include "square.hpp"
-// #include <iostream>
+#include "square.hpp"
+#include <iostream>
 
-// bool testCenter(){
-//     Vector2D cent(2, 3);
-//     Square sq(cent);
-//     return sq.get_center() == cent;
-// }
+bool testCenter(){
+    Vector2D cent(2, 3);
+    Square sq(cent);
+    return sq.get_center() == cent;
+}
 
-// bool testOccupied(){
-//     Vector2D cent(2, 3), cent2(4, 5), cent3(6, 7);
-//     Square sq(cent), sq2(cent2), sq3(cent3);
-//     sq.occupy_by_grass();
-//     sq2.occupy_by_road();
-//     sq3.occupy_by_tower();
-//     return sq.get_occupied() == grass && sq2.get_occupied() == road && sq3.get_occupied() == tower;
-// }
+bool testOccupied(){
+    Vector2D cent(2, 3), cent2(4, 5), cent3(6, 7);
+    Square sq(cent), sq2(cent2), sq3(cent3);
+    sq.occupy_by_grass();
+    sq2.occupy_by_road();
+    sq3.occupy_by_tower();
+    return sq.get_occupied() == grass && sq2.get_occupied() == road && sq3.get_occupied() == tower;
+}
 
-// static int square_test() {
-//     int fails = 0;
+static int square_test() {
+    int fails = 0;
 
-//     if (testCenter()){
-//         std::cout << "testCenter: Passed" << std::endl;
-//     } else {
-//         std::cout << "testCenter: Failed" << std::endl;
-//         fails++;
-//     }
+    if (testCenter()){
+        std::cout << "testCenter: Passed" << std::endl;
+    } else {
+        std::cout << "testCenter: Failed" << std::endl;
+        fails++;
+    }
 
-//     if (testOccupied()){
-//         std::cout << "testOccupied: Passed" << std::endl;
-//     } else {
-//         std::cout << "testOccupied: Failed" << std::endl;
-//         fails++;
-//     }
+    if (testOccupied()){
+        std::cout << "testOccupied: Passed" << std::endl;
+    } else {
+        std::cout << "testOccupied: Failed" << std::endl;
+        fails++;
+    }
 
-//     if (fails == 0){
-//         std::cout << "All Square test passed" << std::endl;
-//     } else {
-//         std::cout << fails << " Square test failed" << std::endl;
-//     }
+    if (fails == 0){
+        std::cout << "All Square test passed" << std::endl;
+    } else {
+        std::cout << fails << " Square test failed" << std::endl;
+    }
 
-//     return fails;
-// }
+    return fails;
+}
--- tests/main.cpp
+++ tests/main.cpp
@@ -4,12 +4,12 @@
 #include "EnemyTests.cpp"
 
 int main(){
-    // int fails = 0;
-    // std::cout << "\n" <<"RUNNING LEVEL TEST ----------------------------------" << "\n" << std::endl;
-    // fails += level_test();
+    int fails = 0;
+    std::cout << "\n" <<"RUNNING LEVEL TEST ----------------------------------" << "\n" << std::endl;
+    fails += level_test();
 
-    // std::cout << "\n" <<"RUNNING SQUARE TEST ----------------------------------" << "\n" << std::endl;
-    // fails += square_test();
+    std::cout << "\n" <<"RUNNING SQUARE TEST ----------------------------------" << "\n" << std::endl;
+    fails += square_test();
 
     // std::cout << "\n" <<"RUNNING OBJECT TEST ----------------------------------" << "\n" << std::endl;
     // fails += object_tests();
@@ -17,7 +17,7 @@ int main(){
     // std::cout << "\n" <<"RUNNING ENEMY TEST ----------------------------------" << "\n" << std::endl;
     // fails += enemy_tests();
 
-    // std::cout << "\n" <<"TEST FINISHED WITH: " << fails << " ERRORS" << "\n" << std::endl;
+    std::cout << "\n" <<"TEST FINISHED WITH: " << fails << " ERRORS" << "\n" << std::endl;
 
     return 0;
 }
