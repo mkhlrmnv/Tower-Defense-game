dbab621e7760471e2ec13cd2711d1aa4e0259fa9
Mikhail Romanov <mikhail.romanov@aalto.fi>
2023-11-30 15:45:56 +0200

    added doxygen comments

--- src/renderer.hpp
+++ src/renderer.hpp
@@ -15,66 +15,125 @@
 
 /**
  * @brief Class for creating drawable game objects for window.draw([DRAWABLE GAME OBJECT])
- * 
  */
 class Renderer{
 public:
-
+    /**
+     * @brief Construct a new Renderer object
+     */
     Renderer();
+    /**
+     * @brief Destroy the Renderer object
+     */
     ~Renderer(){}
     Renderer(const Renderer& ) = delete;
     Renderer operator=(const Renderer&) = delete;
 
-    // call at the beginning of the game
+    /**
+     * @brief Makes displays current level
+     * @param lv level that is displayed
+     */
     void make_drawable_level(Level & lv); 
     void make_level_info_texts(int game_resolution, int side_bar_width);
     
-    // draw background
+    /**
+     * @brief draw background
+     * @param rwindow windows where background is drawn
+     */
     void draw_level(sf::RenderWindow& rwindow);
 
-    // draw single enemy  
+    /**
+     * @brief draw single enemy  
+     * @param rwindow window where to draw
+     * @param e_ptr pointer to enemy
+     * @param frame current frame of animation
+     * @param move_animation current frame for enemies move animation (for smoother movement)
+     */
     void draw_enemy(sf::RenderWindow& rwindow, Enemy* e_ptr, int frame, int move_animation);
     
-    // draw enemies on from a list
+    /**
+     * @brief draw enemies on from a list
+     * @param rwindow window where to draw
+     * @param enemies list of enemies
+     * @param frame current frame of animation
+     * @param move_animation current frame for enemies move animation (for smoother movement)
+     */
     void draw_enemies(sf::RenderWindow& rwindow, std::vector< Enemy * > enemies, int frame, int move_animation);
 
-    // draw single enemy 
+    /**
+     * @brief draw single enemy 
+     * @param rwindow window where to draw
+     * @param t_ptr pointer to tower
+     * @param frame current frame of animation
+     */
     void draw_tower(sf::RenderWindow& rwindow, Tower* t_ptr, int frame);
     
-    // draw towers on from a list
+    /**
+     * @brief draw towers on from a list
+     * @param rwindow  window where to draw
+     * @param towers list of towers
+     * @param frame current frame of animation
+     */
     void draw_towers(sf::RenderWindow& rwindow, std::vector< Tower * > towers, int frame);
 
-
-    // draw texts 
 private:
-    // a sprite for drawing grid
+    /**
+     * @brief a sprite for drawing grid
+     */
     sf::Sprite _drawable_level;
 
     // a sprite for drawing objects 
+    /**
+     * @brief a sprite for drawing objects 
+     */
     sf::Sprite _drawable_enemy;
+    /**
+     * @brief a sprite for drawing objects 
+     */
     sf::Sprite _drawable_tower; 
 
-    // grids connected as a one RenderedTexture
+    /**
+     * @brief grids connected as a one RenderedTexture
+     */
     sf::RenderTexture _level_texture; 
 
-    // place holders
+    /**
+     * @brief place holders
+     */
     sf::RenderTexture _tower_texture; 
+    /**
+     * @brief place holders
+     */
     sf::RenderTexture _enemy_texture; 
 
-    float scale_factor; // number that scales textures to right size
+    /**
+     * @brief number that scales textures to right size
+     * 
+     */
+    float scale_factor;
 
-    sf::Texture _tower_sprite; // for tower texture
-    sf::Texture _enemy_sprite; // for enemy texture
+    /**
+     * @brief for tower texture
+     */
+    sf::Texture _tower_sprite;
+    /**
+     * @brief for enemy texture
+     */
+    sf::Texture _enemy_sprite;
 
     sf::Texture _grass_pic;
     sf::Texture _road_pic;
     sf::Texture _house_pic;
 
+    /**
+     * @brief Scale for towers
+     */
     float _scale_factor_tower = 2.5; // Adjust this value as needed
     float _scale_factor_enemy = 1; // TODO: some enemy type depending value
 
-
-    // Resource handler
+    /**
+     * @brief pointer to Resource handler
+     */
     ResourceHandler _rh;
 };
 
