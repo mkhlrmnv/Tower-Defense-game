4c0dddf8917545c8a090654dc774abacab26f387
Mikhail Romanov <mikhail.romanov@aalto.fi>
2023-12-01 12:52:35 +0200

    added so lives dont go to negative values, and so new round doesn't start if player has 0 lives

--- src/level.cpp
+++ src/level.cpp
@@ -87,7 +87,9 @@ void Level::add_lives(int how_much){
 }
 
 void Level::take_lives(int how_much){
-    _lives -= how_much;
+    if ((_lives - how_much) >= 0){
+        _lives -= how_much;
+    }
 }
 
 std::vector<Enemy*> Level::get_enemies() const{
