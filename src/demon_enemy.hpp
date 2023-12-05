337629368fb8ddb1629addd4d424675cfc635fd7
Leo Teodosin <leo.teodosin@aalto.fi>
2023-12-05 16:36:55 +0200

    Add comments to all object files

--- src/demon_enemy.hpp
+++ src/demon_enemy.hpp
@@ -3,19 +3,40 @@
 
 #include "enemy.hpp"
 #include "attack_types.hpp"
-// #include "level.hpp"
 
-class Demon: public Enemy {
+/**
+ * Class for a Demon enemy
+*/
+class Demon : public Enemy {
 public:
+    /**
+     * @brief Constructor to initialize a Demon Enemy object.
+     * 
+     * @param level Reference to the Level object.
+     * @param position Initial position of the Demon Enemy (Vector2D).
+     * @param health Initial health points of the Demon Enemy.
+     * @param damage Damage inflicted by the Demon Enemy.
+     * @param range Range of attack for the Demon Enemy.
+     * @param attack_speed Speed of attack for the Demon Enemy.
+     * @param type Type of the Demon Enemy.
+     * @param speed Speed attribute of the Demon Enemy.
+     * @param defense Defense attribute of the Demon Enemy.
+     * @param size Size of the Demon Enemy.
+     */
+    Demon(Level& level, Vector2D& position, int health = 80, int damage = 5, int range = 130, int attack_speed = 1,
+          int type = ObjectTypes::NoobDemon_CanAttack, int speed = 2, int defense = 5, int size = 3);
 
-    Demon(Level& level, Vector2D& position, int health = 80, int damage = 5, int range = 130, int attack_speed = 1, 
-        int type = ObjectTypes::NoobDemon_CanAttack, int speed = 2, int defense = 5, int size = 3);
-
+    /**
+     * @brief Destructor for the Demon Enemy.
+     */
     ~Demon() { }
 
+    /**
+     * @brief Perform an attack action specific to the Demon Enemy.
+     * 
+     * @return true if the attack is successful, false otherwise.
+     */
     bool attack();
-
-    // void move();
 };
 
 #endif
--- src/tank_enemy.hpp
+++ src/tank_enemy.hpp
@@ -3,19 +3,40 @@
 
 #include "enemy.hpp"
 #include "attack_types.hpp"
-// #include "level.hpp"
 
-class Tank: public Enemy {
+/**
+ * @brief Class for a Tank enemy
+*/
+class Tank : public Enemy {
 public:
+    /**
+     * @brief Constructor to initialize a Tank Enemy object.
+     * 
+     * @param level Reference to the Level object.
+     * @param position Initial position of the Tank Enemy (Vector2D).
+     * @param health Initial health points of the Tank Enemy.
+     * @param damage Damage inflicted by the Tank Enemy.
+     * @param range Range of attack for the Tank Enemy.
+     * @param attack_speed Speed of attack for the Tank Enemy.
+     * @param type Type of the Tank Enemy.
+     * @param speed Speed attribute of the Tank Enemy.
+     * @param defense Defense attribute of the Tank Enemy.
+     * @param size Size of the Tank Enemy.
+     */
+    Tank(Level& level, Vector2D& position, int health = 300, int damage = 5, int range = 80, int attack_speed = 1,
+         int type = ObjectTypes::TankOrc, int speed = 1, int defense = 100, int size = 4);
 
-    Tank(Level& level, Vector2D& position, int health = 300, int damage = 5, int range = 80, int attack_speed = 1, 
-        int type = ObjectTypes::TankOrc, int speed = 1, int defense = 100, int size = 4);
-
+    /**
+     * @brief Destructor for the Tank Enemy.
+     */
     ~Tank() { }
 
+    /**
+     * @brief Perform an attack action specific to the Tank Enemy.
+     * 
+     * @return true if the attack is successful, false otherwise.
+     */
     bool attack();
-
-    // void move();
 };
 
 #endif
