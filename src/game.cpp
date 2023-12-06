5c9fde3487c5cca373d48da9974cfcd1e7e8509c
Mikhail Romanov <mikhail.romanov@aalto.fi>
2023-12-06 22:25:12 +0200

    added enemy dependand size to rendering

--- src/game.cpp
+++ src/game.cpp
@@ -335,9 +335,25 @@ void Game::render(){
 void Game::start_round(){
     _round_over = false;
     //Spawns round played ammount multiplied by difficulty level amount of enemies
-    for (int i = 0; i < (_difficulty_multiplier * _level.get_round()); i++)
+    // for (int i = 0; i < (_difficulty_multiplier * _level.get_round()); i++)
+    // {
+    //     // Square where enemies can spawn
+    //     Square* spawn_sq = _level.get_first_road();
+    //     // Picks random type of enemy from available types
+    //     int rand_types = rand() % _available_types;
+    //     // Picks random position inside spawn square
+    //     int x = rand() % 80;
+    //     int y = rand() % 40;
+    //     Vector2D rand_pos = Vector2D(spawn_sq->get_center().x - (_level.get_square_size() / 2) + x, 1 + y);
+    //     // add enemy to level
+    //     _level.add_enemy_by_type(rand_types, rand_pos);
+    // }
+    // // If available types are under 8, adds new type
+    // if (_available_types < 7){
+    //     _available_types++;
+    // }
+    for (int i = 0; i < 8; i++)
     {
-        // Square where enemies can spawn
         Square* spawn_sq = _level.get_first_road();
         // Picks random type of enemy from available types
         int rand_types = rand() % _available_types;
@@ -346,12 +362,9 @@ void Game::start_round(){
         int y = rand() % 40;
         Vector2D rand_pos = Vector2D(spawn_sq->get_center().x - (_level.get_square_size() / 2) + x, 1 + y);
         // add enemy to level
-        _level.add_enemy_by_type(rand_types, rand_pos);
+        _level.add_enemy_by_type(i, rand_pos);
     }
-    // If available types are under 8, adds new type
-    if (_available_types < 7){
-        _available_types++;
-    }
+    
 }
 
 // updates all enemies
