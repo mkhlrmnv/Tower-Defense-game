d861fcd4aa80975721b6e783f7e2a5dbc9680d0d
Leo Teodosin <leo.teodosin@aalto.fi>
2023-12-06 18:42:02 +0200

    Add variable for object wait time

--- src/tank_enemy.cpp
+++ src/tank_enemy.cpp
@@ -5,7 +5,7 @@ Tank::Tank(Level& level, Vector2D& position, int health, int damage, int range, 
     Enemy(level, position, health, damage, range, attack_speed, type, speed, defense, size) {}
 
 bool Tank::attack() {
-    if (get_reset_counter() >= 50) {
+    if (get_reset_counter() >= get_wait_time()) {
         set_attack_speed(get_original_attack_speed());
         set_speed(get_original_speed());
         set_reset_counter(0);
