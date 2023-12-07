13ecf552fcf051362adeb0b65994653d1e0d5f10
Mikhail Romanov <mikhail.romanov@aalto.fi>
2023-12-07 17:13:36 +0200

    fixed bug with fast boy not drawing

--- src/fastboy_enemy.hpp
+++ src/fastboy_enemy.hpp
@@ -24,7 +24,7 @@ public:
      * @param size Size of the Fast Boy Enemy.
      */
     Fast_Boy(Level& level, Vector2D& position, int health = 20, int damage = 0, int range = 0, int attack_speed = 1,
-             int type = ObjectTypes::FastBoy, int speed = 10, int defense = 5, int size = 1);
+             int type = ObjectTypes::FastBoy, int speed = 10, int defense = 5, int size = 2);
 
     /**
      * @brief Destructor for the Fast Boy Enemy.
